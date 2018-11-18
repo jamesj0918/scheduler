d747f614b4ef991bb8132d10f78a11e3f8d1b84b
James <jamesj0107@naver.com>
2018-11-19 03:03:49 +0900

    Inital Commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import SuiVue from 'semantic-ui-vue'
+import '../semantic/dist/semantic.min.css';
+import VueAgile from 'vue-agile'
+
+Vue.use(SuiVue);
+Vue.use(VueAgile);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/SearchForm.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div>
+        <form-table id="formTable"></form-table>
+    </div>
+</template>
+
+<script>
+    import FormTable from './FormTable'
+    export default {
+        name: 'SearchForm',
+        components:{
+            'form-table': FormTable,
+        }
+
+    };
+</script>
+
+<style scoped>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    #formTable{
+        width: 80%;
+        margin: auto;
+    }
+</style>
