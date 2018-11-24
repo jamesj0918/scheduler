4ccca5c662909e0eb86424230b0929c1fe1f6ddd
James <jamesj0107@naver.com>
2018-11-25 00:04:40 +0900

    Add subcategory fields and let users link to SubjectList component

---
+++ src/views/Subcategory.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <div id="categoryWrap" >
+            <div v-for="(subcategory, key) in subcategories" :key="key">
+                <div v-if="key === category">
+                    <div v-for="subject in subcategory" :key="subject">
+                        <div class="item">
+                            <div class="title">
+                                {{subject}}
+                            </div>
+                            <div class="categoryInfo">
+                                <div class="num">100개의 강의</div>
+                                <div class="iconWrap" @click="link_subjects(subject)" > <i class="angle right large grey icon"></i></div>
+                            </div>
+                        </div>
+                        <div class="ui divider"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Subcategory",
+        data(){
+            return{
+                category: this.$route.params.category,
+                subcategories:{
+                    상경계: ['경제', '경영', '마케팅/세일즈'],
+                    인문학: ['철학', '문학', '역사/문화'],
+                    사회과학: ['행정', '심리', '교육학', '법', '사회학', '언론/신문/방송'],
+                    국방_군사_경찰: ['국방/군사/경찰'],
+                    과학_공학: ['기계/전기/전자', '도시/토목/건설', '물리학', '생물학', '수학', '천문/지구과학', '화학', '컴퓨터'],
+                    예술: ['무용', '미술', '음악', '연극/영화', '대중문화'],
+                    언어: ['국어', '영어', '일본어', '중국어'],
+                    진로: ['창업/취업', '진로', '논술/면접대비', '공무원/자격증', '고시', '자기능력계발'],
+                    취미_생활: ['리빙', '레저/스포츠', '여성/패션']
+                },
+            }
+        },
+        methods:{
+            link_subjects(subject){
+                if (subject.indexOf("/") !== -1) {
+                    subject = subject.replace("/", "_").replace("/", "_");
+                }
+                this.$router.push('/result/'+this.category+'/'+subject);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    #categoryWrap{
+        width: 100%;
+        height: 45vh;
+    }
+    .divider{
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    .divider:last-child{
+    }
+    .categoryInfo{
+        height: 100%;
+        width:50%;
+        float: right;
+    }
+    .item{
+        margin-top: 10px;
+        margin-bottom: 10px;
+        display: flex;
+        height: 30px;
+        align-items:center;
+    }
+
+    .title{
+        height: 100%;
+        display: flex;
+        width: 50%;
+        float: left;
+        padding-left: 20px;
+        font-size: 15px;
+        text-align: left;
+        font-weight: bold;
+        align-items:center;
+    }
+    .num{
+        height: 100%;
+        display: flex;
+        align-items:center;
+        width: 80%;
+        text-align: right;
+        padding-right:10px;
+        float: left;
+    }
+    .iconWrap{
+        display: flex;
+        width: 20%;
+        height: 100%;
+        float: right;
+        justify-content:center;
+        align-items:center;
+        cursor: pointer;
+    }
+</style>
